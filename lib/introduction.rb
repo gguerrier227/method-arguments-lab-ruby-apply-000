1809b4f233c198dc4edb124beda6f3c11e7fd5f7
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-08-23 13:46:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,6 +8,6 @@ def introduction_with_language (arg1,arg2)
   puts "Hi, my name is #{arg1[0]} and I am learning to program in #{arg2[0]}.\n"
 end
 
-name = ['Dan']
-language = ['Ember.js']
+name = ['D']
+language = ['E']
 introduction_with_language(name,language)

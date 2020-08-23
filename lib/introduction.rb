ec96cd6f5e37bff848988d1fc268feed95d066ac
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-08-23 13:45:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,7 @@ end
 introduction ('Josh')
 
 def introduction_with_language (arg1,arg2)
-  puts "Hi, my name is #{arg1[0]} and I am learning to program in #{arg2[1]}.\n"
+  puts "Hi, my name is #{arg1[0]} and I am learning to program in #{arg2[0]}.\n"
 end
 
 name = ['Dan']
